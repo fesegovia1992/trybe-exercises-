0c6f2ab2e1ac4dce393787c42855ccb530e2f446
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-10-27 00:40:24 -0300

    Adiciona resolucao exercise 2

---
+++ exercises/introducao-a-javascript-e-logica-de-progamacao_1/exercise_2.js
@@ -0,0 +1,13 @@
+/*Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.*/
+
+let valorMaior = max(33,51);
+console.log(valorMaior);
+
+
+function max(a,b){
+    if (a > b)
+        return a;
+        else return b;
+
+}
+
