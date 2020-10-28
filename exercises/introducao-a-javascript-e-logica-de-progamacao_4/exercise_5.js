015d7ec1bab4f3596a4d1e72223a386256006767
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-10-28 18:49:58 -0300

    Adiciona resolucao do exercise 5

---
+++ exercises/introducao-a-javascript-e-logica-de-progamacao_4/exercise_5.js
@@ -0,0 +1,19 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+};
+
+
+let info2 = {
+
+}
+
+info2['personagens:'] = "Margarida e Tio Patinhas";
+info2['origem:'] = "Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178";
+info2['nota:'] = "Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas";
+info2['recorrentes:'] = "Sim";
+
+
+console.log(info2);
