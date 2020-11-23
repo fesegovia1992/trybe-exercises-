4439888fc1f3d0f519aaea8218b534dfaa9db392
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-11-23 19:20:57 -0300

    Adiciona segunda parte do exercise 1

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercise1_partI.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercise1_partI.js
@@ -1,3 +1,4 @@
+//Ex 1 parte I
 const testingScope = (escopo) => { 
     if (escopo === true) { 
       let ifScope = `Não devo ser utilizada fora do meu escopo (if).`;
@@ -10,3 +11,13 @@ const testingScope = (escopo) => {
   }
 
   testingScope(true);
+
+  //Ex 1 parte II
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  const ordemCrescente = () => []
+    oddsAndEvens.sort(function(a, b) {
+        return a - b;
+    });
+  
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
