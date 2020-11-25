a3e3bb47869b39eb8fc662a1b454ee1e9ebb86ae
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-11-24 23:35:39 -0300

    Add exercise part II until ex 5

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercises.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercises.js
@@ -140,3 +140,63 @@ const order = {
 }
   
   orderModifier(order);
+
+  /* Exercicio Part II */
+
+  const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  //Crie uma função para adicionar o turno da manhã na lesson2. 
+  //Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+  /*const addPeriod = (obj, key, value) => {
+    obj[key] = value;
+    
+  }
+  addPeriod(lesson2, 'turno', 'manhã');
+  console.log(lesson2);*/
+
+  //Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+  const list = obj => {
+      const arrayList = Object.keys(obj);
+      for(i in arrayList) {
+        console.log(`${arrayList[i]}`);
+      }
+  }
+  list(lesson3);
+  
+  //Crie uma função para mostrar o tamanho de um objeto.
+  const objLength = obj => Object.keys(obj).length;
+  console.log(objLength(lesson3))
+
+  //Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+  const objValues = obj => Object.values(obj);
+  console.log(objValues(lesson3));
+
+  //Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, 
+  //sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+  const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+  console.log(allLessons);
+  
+  //Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+  const totalStudents = (obj) => Object.entries(obj);
+  
+  console.log(totalStudents(allLessons))
+  
