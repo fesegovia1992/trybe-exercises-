103879a43e5aca959940fcd535e3dbf0a79d5717
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-12-03 14:30:02 -0300

    Exercise 2

---
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise2.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+
+// escreva sum abaixo
+const sum = (...numbers) => {
+    let result =0;
+    numbers.forEach((number) => result += number)
+    return result;
+}
+console.log(sum(1,2,3,4));
+assert.strictEqual(sum(), 0)
+assert.strictEqual(sum(1), 1)
+assert.strictEqual(sum(1, 2), 3)
+assert.strictEqual(sum(1, 2, 3), 6)
+assert.strictEqual(sum(1, 2, 3, 4), 10)
